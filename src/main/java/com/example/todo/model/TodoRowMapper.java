39bf505b130c2897579474b64823dc7195685ed5
Purkha Ram <pkchoudhary09@yahoo.com>
2023-08-19 05:42:29 +0000

    commit message

---
+++ src/main/java/com/example/todo/model/TodoRowMapper.java
@@ -0,0 +1,36 @@
+package com.example.todo.model;
+
+import com.example.todo.model.Todo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
+
+
+public class TodoRowMapper implements RowMapper<Todo>{
+
+	@Override
+	public Todo mapRow(ResultSet rs, int rowNum) throws SQLException {
+		
+	return new Todo(
+            rs.getInt("id"),
+            rs.getString("todo"),
+            rs.getString("priority"),
+            rs.getString("status"));
+            
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
